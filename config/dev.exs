b12b74fb5d700528a8c09d41c25c4a1eea2124c4
Fabricio Damazio <fabridamazio@gmail.com>
2022-03-06 13:05:26 -0300

    Add tailwindcss

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,8 @@ config :mibli, MibliWeb.Endpoint,
   secret_key_base: "mh//N4O1Zqt5UzXKj6OezojdjZg30Udhtq+SFqP8/tAlQNw/hibsC879l18VUNNF",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
