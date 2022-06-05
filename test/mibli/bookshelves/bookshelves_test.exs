2138e93415a9c2aef704d25da811cec63fc21f45
FabriDamazio <fabridamazio@gmail.com>
2022-06-04 21:00:21 -0300

    Clean bookshelves_test code

--- test/mibli/bookshelves/bookshelves_test.exs
+++ test/mibli/bookshelves/bookshelves_test.exs
@@ -1,5 +1,5 @@
 defmodule Mibli.BookshelvesTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   import Ecto.Query
 
@@ -8,61 +8,50 @@ defmodule Mibli.BookshelvesTest do
   alias Mibli.Bookshelves.Book
   alias Mibli.AccountsFixtures
 
-  @book_params %{
-    title: "Title",
-    author: "Author",
-    publisher: "Publisher",
-    total_pages: 1,
-    isbn: "1",
-    user_id: nil
-  }
-
   setup do
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+    Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+    user = AccountsFixtures.user_fixture()
+
+    %{
+      book: %{
+        title: "Title",
+        author: "Author",
+        publisher: "Publisher",
+        total_pages: 1,
+        isbn: "1",
+        user_id: user.id
+      }
+    }
   end
 
   describe "add_book/1" do
-    test "adds the book to the user bookshelf on success" do
-      user = AccountsFixtures.user_fixture()
-      valid_book = %{@book_params | user_id: user.id}
-
-      {:ok, %Book{} = book} = Bookshelves.add_book(valid_book)
+    test "adds the book to the user bookshelf", %{book: book} do
+      {:ok, book_added} = Bookshelves.add_book(book)
 
       assert [1] == Repo.all(from b in Book, select: count())
-      assert book == Repo.get_by!(Book, id: book.id, title: valid_book.title, user_id: user.id)
+      assert book_added == Repo.get_by!(Book, id: book_added.id)
     end
 
-    test "book title is a required" do
-      user = AccountsFixtures.user_fixture()
-      nil_title_book = %{@book_params | title: nil, user_id: user.id}
-
-      {:error, changeset} = Bookshelves.add_book(nil_title_book)
+    test "book title is a required", %{book: book} do
+      {:error, changeset} = Bookshelves.add_book(%{book | title: nil})
 
       assert [title: {"can't be blank", [validation: :required]}] == changeset.errors
     end
 
-    test "book title can't be blank" do
-      user = AccountsFixtures.user_fixture()
-      blank_title_book = %{@book_params | title: "", user_id: user.id}
-
-      {:error, changeset} = Bookshelves.add_book(blank_title_book)
+    test "book title can't be blank", %{book: book} do
+      {:error, changeset} = Bookshelves.add_book(%{book | title: ""})
 
       assert [title: {"can't be blank", [validation: :required]}] == changeset.errors
     end
 
-    test "book title can't be only white space" do
-      user = AccountsFixtures.user_fixture()
-      whitespace_title_book = %{@book_params | title: " ", user_id: user.id}
-
-      {:error, changeset} = Bookshelves.add_book(whitespace_title_book)
+    test "book title can't be only white spaces", %{book: book} do
+      {:error, changeset} = Bookshelves.add_book(%{book | title: " "})
 
       assert [title: {"can't be blank", [validation: :required]}] == changeset.errors
     end
 
-    test "book should belongs to an user" do
-      book_without_user = %{@book_params | user_id: nil}
-
-      {:error, changeset} = Bookshelves.add_book(book_without_user)
+    test "book should belongs to an user", %{book: book} do
+      {:error, changeset} = Bookshelves.add_book(%{book | user_id: nil})
 
       assert [user_id: {"can't be blank", [validation: :required]}] == changeset.errors
     end
