ae397cee385ef0a624e9201153de0d4af4ea3363
Fabricio Damazio <fabridamazio@gmail.com>
2024-03-18 20:23:52 -0300

    Create folder for books related liveviews

--- src/mibli/lib/mibli_web/live/books/books_live.ex
+++ src/mibli/lib/mibli_web/live/books/books_live.ex
@@ -1,16 +1,23 @@
-defmodule MibliWeb.BooksLive do
+defmodule MibliWeb.Books.BooksLive do
   use MibliWeb, :live_view
   alias Mibli.Books
 
+  @impl true
   def mount(_params, _session, socket) do
     socket = assign(socket, books: Books.get_books())
 
     {:ok, socket}
   end
 
+  @impl true
   def render(assigns) do
     ~H"""
       <h1 class="text-slate-600 text-5xl mb-12">My Books</h1>
+      <button class="mt-6 bg-sky-600 hover:bg-sky-700 text-white px-4 py-2 rounded">
+        <.link href={~p"/books/new"}>
+        Add Book
+        </.link>
+      </button>
       <div>
         <ul class="flex flex-row flex-wrap">
           <li :for={book <- @books} class="min-w-80">
