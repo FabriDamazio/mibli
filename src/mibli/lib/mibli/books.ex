4cad3edc3d640a68d2cd422d2c32b0ccf652e482
Fabricio Damazio <fabridamazio@gmail.com>
2024-03-18 18:59:06 -0300

    Stub Books imp to work on a very basic books_live layout

---
+++ src/mibli/lib/mibli/books.ex
@@ -0,0 +1,35 @@
+defmodule Mibli.Books do
+  def get_books() do
+    [%{
+      title: "Programming Phoenix",
+      author: "Fabricio Damazio",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
+    },
+    %{
+      title: "Programming Elixir",
+      author: "Fabricio Damazio",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
+    },
+    %{
+      title: "Programming Elixir",
+      author: "Fabricio Damazio",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
+    },
+    %{
+      title: "Programming Elixir",
+      author: "Fabricio Damazio",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
+    },
+    %{
+      title: "Programming Elixir",
+      author: "Fabricio Damazio",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
+    },
+  ]
+  end
+end
--- src/mibli/lib/mibli_web/live/books_live.ex
+++ src/mibli/lib/mibli_web/live/books_live.ex
@@ -1,13 +1,32 @@
 defmodule MibliWeb.BooksLive do
   use MibliWeb, :live_view
+  alias Mibli.Books
 
   def mount(_params, _session, socket) do
+    socket = assign(socket, books: Books.get_books())
+
     {:ok, socket}
   end
 
   def render(assigns) do
     ~H"""
-      <p>Books Live</p>
+      <h1 class="text-slate-600 text-5xl mb-12">My Books</h1>
+      <div>
+        <ul class="flex flex-row flex-wrap">
+          <li :for={book <- @books} class="min-w-80">
+            <section class="max-w-sm rounded overflow-hidden shadow-lg m-4 ">
+              <article class="px-6 py-4">
+                <h3 class="text-slate-600 font-bold text-xl mb-2">
+                  <%= book.title %>
+                </h3>
+                <p class="text-slate-500 text-base">
+                  <%= book.description %>
+                </p>
+              </article>
+            </section>
+          </li>
+        </ul>
+      </div>
     """
   end
 end
