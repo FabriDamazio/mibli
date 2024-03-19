9aeba2e15b3c71a66145d5186777c39ae9bf1dcb
Fabricio Damazio <fabridamazio@gmail.com>
2024-03-18 21:45:45 -0300

    Mix format

--- src/mibli/lib/mibli/books.ex
+++ src/mibli/lib/mibli/books.ex
@@ -1,35 +1,36 @@
 defmodule Mibli.Books do
   def get_books() do
-    [%{
-      title: "Programming Phoenix",
-      author: "Fabricio Damazio",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    [
+      %{
+        title: "Programming Phoenix",
+        author: "Fabricio Damazio",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
       Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
-    },
-    %{
-      title: "Programming Elixir",
-      author: "Fabricio Damazio",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      },
+      %{
+        title: "Programming Elixir",
+        author: "Fabricio Damazio",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
       Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
-    },
-    %{
-      title: "Programming Elixir",
-      author: "Fabricio Damazio",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      },
+      %{
+        title: "Programming Elixir",
+        author: "Fabricio Damazio",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
       Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
-    },
-    %{
-      title: "Programming Elixir",
-      author: "Fabricio Damazio",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      },
+      %{
+        title: "Programming Elixir",
+        author: "Fabricio Damazio",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
       Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
-    },
-    %{
-      title: "Programming Elixir",
-      author: "Fabricio Damazio",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      },
+      %{
+        title: "Programming Elixir",
+        author: "Fabricio Damazio",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.
       Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
-    },
-  ]
+      }
+    ]
   end
 end
--- src/mibli/lib/mibli_web/live/books/books_live.ex
+++ src/mibli/lib/mibli_web/live/books/books_live.ex
@@ -12,28 +12,28 @@ defmodule MibliWeb.Books.BooksLive do
   @impl true
   def render(assigns) do
     ~H"""
-      <h1 class="text-slate-600 text-5xl mb-12">My Books</h1>
-      <button class="mt-6 bg-sky-600 hover:bg-sky-700 text-white px-4 py-2 rounded">
-        <.link href={~p"/books/new"}>
+    <h1 class="text-slate-600 text-5xl mb-12">My Books</h1>
+    <button class="mt-6 bg-sky-600 hover:bg-sky-700 text-white px-4 py-2 rounded">
+      <.link href={~p"/books/new"}>
         Add Book
-        </.link>
-      </button>
-      <div>
-        <ul class="flex flex-row flex-wrap">
-          <li :for={book <- @books} class="min-w-80">
-            <section class="max-w-sm rounded overflow-hidden shadow-lg m-4 ">
-              <article class="px-6 py-4">
-                <h3 class="text-slate-600 font-bold text-xl mb-2">
-                  <%= book.title %>
-                </h3>
-                <p class="text-slate-500 text-base">
-                  <%= book.description %>
-                </p>
-              </article>
-            </section>
-          </li>
-        </ul>
-      </div>
+      </.link>
+    </button>
+    <div>
+      <ul class="flex flex-row flex-wrap">
+        <li :for={book <- @books} class="min-w-80">
+          <section class="max-w-sm rounded overflow-hidden shadow-lg m-4 ">
+            <article class="px-6 py-4">
+              <h3 class="text-slate-600 font-bold text-xl mb-2">
+                <%= book.title %>
+              </h3>
+              <p class="text-slate-500 text-base">
+                <%= book.description %>
+              </p>
+            </article>
+          </section>
+        </li>
+      </ul>
+    </div>
     """
   end
 end
