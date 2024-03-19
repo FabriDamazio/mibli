b6637f4590a182e19d59642737b44650e6373fa3
Fabricio Damazio <fabridamazio@gmail.com>
2024-03-18 21:46:10 -0300

    Change form tag in new_book_live

--- src/mibli/lib/mibli_web/live/books/new_book_live.ex
+++ src/mibli/lib/mibli_web/live/books/new_book_live.ex
@@ -12,34 +12,47 @@ defmodule MibliWeb.Books.NewBookLive do
     <div>
       <h1 class="text-slate-600 text-5xl mb-12">Add Book</h1>
       <div class="flex flex-col">
-        <.form phx-submit="submit_book">
+        <form phx-submit="submit_book">
           <div class="flex flex-col">
             <label for="title" class="text-slate-600 text-lg mb-2">Title</label>
-            <input id="title" name="title" type="text" class="border border-slate-300 rounded px-4 py-2 mb-4">
+            <input
+              id="title"
+              name="title"
+              type="text"
+              class="border border-slate-300 rounded px-4 py-2 mb-4"
+            />
           </div>
           <div class="flex flex-col">
             <label for="author" class="text-slate-600 text-lg mb-2">Author</label>
-            <input id="author" name="author" type="text" class="border border-slate-300 rounded px-4 py-2 mb-4">
+            <input
+              id="author"
+              name="author"
+              type="text"
+              class="border border-slate-300 rounded px-4 py-2 mb-4"
+            />
           </div>
           <div class="flex flex-col">
             <label for="description" class="text-slate-600 text-lg mb-2">Description</label>
-            <textarea id="description" name="description" class="border border-slate-300 rounded px-4 py-2 mb-4"></textarea>
+            <textarea
+              id="description"
+              name="description"
+              class="border border-slate-300 rounded px-4 py-2 mb-4"
+            ></textarea>
           </div>
           <button class="bg-sky-600 hover:bg-sky-700 text-white px-4 py-2 rounded">
             Add Book
           </button>
-        </.form>
+        </form>
       </div>
     </div>
     """
   end
 
-  def handle_event("submit_book", %{"title" => title, "author" => author, "description" => description}, socket) do
-    IO.inspect({title, author, description})
-
-    {:noreply, socket
-    |> put_flash(:info, "Book added successfully")
-    |> push_redirect(to: "/books")}
+  @impl true
+  def handle_event("submit_book", _params, socket) do
+    {:noreply,
+     socket
+     |> put_flash(:info, "Book added successfully")
+     |> push_redirect(to: "/books")}
   end
-
 end
