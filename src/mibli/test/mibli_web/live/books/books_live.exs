d828e0adb9f3ff5d81f2bdb7bddb7a565cc16873
Fabricio Damazio <fabridamazio@gmail.com>
2024-04-23 20:47:01 -0300

    Testing for book editing modal

--- src/mibli/test/mibli_web/live/books/books_live.exs
+++ src/mibli/test/mibli_web/live/books/books_live.exs
@@ -2,13 +2,14 @@ defmodule MibliWeb.BooksLiveTest do
   use MibliWeb.ConnCase, async: true
 
   import Phoenix.LiveViewTest
-  import Mibli.AccountsFixtures
+  alias Mibli.AccountsFixtures
+  alias Mibli.BooksFixtures
 
   describe "Books page" do
     test "renders books page", %{conn: conn} do
       {:ok, lv, html} =
         conn
-        |> log_in_user(user_fixture())
+        |> log_in_user(AccountsFixtures.user_fixture())
         |> live(~p"/books")
 
       assert %{module: MibliWeb.Books.BooksLive} = lv
@@ -21,6 +22,72 @@ defmodule MibliWeb.BooksLiveTest do
       assert {:redirect, %{to: path, flash: flash}} = redirect
       assert path == ~p"/users/log_in"
       assert %{"error" => "You must log in to access this page."} = flash
+    end
+  end
+
+  describe "edit book modal" do
+    test "renders edit book modal", %{conn: conn} do
+      user = AccountsFixtures.user_fixture()
+      book = BooksFixtures.book_fixture(%{user_id: user.id})
+
+      {:ok, lv, html} =
+        conn
+        |> log_in_user(user)
+        |> live(~p"/books/edit/#{book.id}")
+
+      assert %{module: MibliWeb.Books.BooksLive} = lv
+      assert html =~ "Edit Book"
+    end
+
+    test "redirects if user is not logged in", %{conn: conn} do
+      assert {:error, redirect} = live(conn, ~p"/books/edit/1")
+
+      assert {:redirect, %{to: path, flash: flash}} = redirect
+      assert path == ~p"/users/log_in"
+      assert %{"error" => "You must log in to access this page."} = flash
+    end
+
+    test "redirects to books list if book does not exists", %{conn: conn} do
+       assert {:error, redirect} =
+        conn
+        |> log_in_user(AccountsFixtures.user_fixture())
+        |> live(~p"/books/edit/1")
+
+      assert {:live_redirect, %{to: path, flash: flash}} = redirect
+      assert path == ~p"/books"
+      assert %{} = flash
+    end
+
+    test "shows flash message when save an edited book", %{conn: conn} do
+      user = AccountsFixtures.user_fixture()
+      book = BooksFixtures.book_fixture(%{user_id: user.id})
+
+      {:ok, lv, _html} =
+        conn
+        |> log_in_user(user)
+        |> live(~p"/books/edit/#{book.id}")
+
+      assert lv
+        |> element("form#modal-form")
+        |> render_submit(%{title: "title"}) =~ "Book saved."
+
+      assert %{module: MibliWeb.Books.BooksLive} = lv
+    end
+
+    test "redirects to book list after saving", %{conn: conn} do
+      user = AccountsFixtures.user_fixture()
+      book = BooksFixtures.book_fixture(%{user_id: user.id})
+
+      {:ok, lv, _html} =
+        conn
+        |> log_in_user(user)
+        |> live(~p"/books/edit/#{book.id}")
+
+        lv
+        |> element("form#modal-form")
+        |> render_submit(%{title: "title"})
+
+      assert_patch(lv, "/books", 30)
     end
   end
 end
