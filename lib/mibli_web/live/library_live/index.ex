0ed294fbab5d561be372d4654bf9814bb615b0ba
FabriDamazio <fabridamazio@gmail.com>
2024-12-04 16:11:22 -0300

    Some visual improvements no book index

--- lib/mibli_web/live/library_live/index.ex
+++ lib/mibli_web/live/library_live/index.ex
@@ -12,10 +12,21 @@ defmodule MibliWeb.LibraryLive.Index do
 
   def render(assigns) do
     ~H"""
-    <h1>HELLO FROM LIBRARY!</h1>
-    <%= for book <- @books do %>
-      <%= book.title %>
-    <% end %>
+    <div class="container mx-auto p-8">
+      <h1 class="text-3xl font-bold mb-6 text-center">Minha Biblioteca</h1>
+
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
+        <%= for book <- @books do %>
+          <div class="bg-white shadow-lg rounded-lg p-6 hover:shadow-2xl transition-shadow duration-300">
+            <h2 class="text-xl font-semibold mb-2"><%= book.title %></h2>
+            <p class="text-gray-700 mb-1"><strong>Author:</strong> <%= book.author %></p>
+            <p class="text-gray-600">
+              <strong>Year:</strong> <%= book.publication_year %>
+            </p>
+          </div>
+        <% end %>
+      </div>
+    </div>
     """
   end
 end
